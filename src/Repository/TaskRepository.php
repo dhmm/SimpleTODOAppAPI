d9a1d1e5d735ee8848b46542794da423a5e0d6ac
unknown <dhmm@dhmm>
2019-06-11 12:24:47 +0300

    tsnaforms to task repo added

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -17,34 +17,23 @@ class TaskRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Task::class);
+    }    
+    public function transform(Task $task) {
+        return [
+            'id' => (int) $task->getId(),
+            'name' => (string) $task->getName(),
+            'done' => (boolean) $task->getDone()
+        ];
     }
 
-    // /**
-    //  * @return Task[] Returns an array of Task objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+    public function transformAll() {
+        $tasks = $this->findAll();
+        $tasksArray = [];
 
-    /*
-    public function findOneBySomeField($value): ?Task
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        foreach($tasks as $task) {
+            $tasksArray[] = $this->transform($task);
+        }
+
+        return $tasksArray;
     }
-    */
 }
